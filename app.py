9b051f7e29cc7d4ed27b6beae89d9fe30b514fe8
Luigi Pellegrino <60132440+PellegrinoLuigi@users.noreply.github.com>
2025-02-18 11:21:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,6 @@ def get_trains():
     except Exception as e:
         return jsonify({"error": str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Usa la porta specificata da Render
+    app.run(host="0.0.0.0", port=port, debug=True)
