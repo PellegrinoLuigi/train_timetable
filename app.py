0cb5365d30dc3d1ccbbf8a5ef406c92eb607df7d
Luigi Pellegrino <60132440+PellegrinoLuigi@users.noreply.github.com>
2025-02-18 10:54:56 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+import datetime
+import pytz
+
+app = Flask(__name__)
+
+def get_italy_current_time():
+    italy_tz = pytz.timezone('Europe/Rome')
+    current_time = datetime.datetime.now(italy_tz)
+    return current_time.isoformat()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_trains', methods=['POST'])
+def get_trains():
+    url = "https://www.lefrecce.it/Channels.Website.BFF.WEB/website/ticket/solutions"
+    body = {
+        "departureLocationId": 830011119,
+        "arrivalLocationId": 830011112,
+        "departureTime": get_italy_current_time(),
+        "adults": 1,
+        "children": 0,
+        "criteria": {
+            "frecceOnly": False,
+            "regionalOnly": True,
+            "noChanges": True,
+            "order": "DEPARTURE_DATE",
+            "limit": 10,
+            "offset": 0
+        },
+        "advancedSearchRequest": {
+            "bestFare": False
+        }
+    }
+    try:
+        response = requests.post(url, json=body)
+        return jsonify(response.json())
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
