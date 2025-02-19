817e8b2485fa394ebd416d26925777cb4576ccdb
Luigi Pellegrino <60132440+PellegrinoLuigi@users.noreply.github.com>
2025-02-19 09:13:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ def get_trains():
     direction = data.get("direction")
     departureLocationId=830011119
     arrivalLocationId=830011112
-    if(direction=='2')
-        temporary=departureLocationId
-        departureLocationId =arrivalLocationId
-        arrivalLocationId= temporary    
+    if direction == "2":  
+        temporary = departureLocationId
+        departureLocationId = arrivalLocationId
+        arrivalLocationId = temporary
     url = "https://www.lefrecce.it/Channels.Website.BFF.WEB/website/ticket/solutions"
     body = {
         "departureLocationId": departureLocationId,
