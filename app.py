39b28ad611911682355338b9d5c7f7fd1bd9f174
lpellegrino <pelle.l33@gmail.com>
2025-02-20 10:13:34 +0100

    add

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pytz
 import os
 
 app = Flask(__name__ , static_folder="static")
+#app.run(debug=True)
 
 # Dizionario con i valori delle stazioni
 stazioni = {
@@ -30,127 +31,39 @@ def get_trains():
     data = request.get_json()  # Riceve i dati come JSON
     station_name_from = data.get("from")
     station_name_to = data.get("to")
-    limit = data.get("limit")
-
-    # URL per fare la richiesta
+    limit=  data.get("limit")
     url = "https://www.lefrecce.it/Channels.Website.BFF.WEB/website/ticket/solutions"
-    
-    # Body della richiesta
     body = {
-        "departureLocationId": stazioni.get(station_name_from, 830011112),  # Default se non trovato
+        "departureLocationId": stazioni.get(station_name_from, 830011112),
         "arrivalLocationId": stazioni.get(station_name_to, 830011112),
         "departureTime": get_italy_current_time(),
         "adults": 1,
         "children": 0,
         "criteria": {
-             "frecceOnly": False,
-             "regionalOnly": True,
-             "intercityOnly": False,
-             "tourismOnly": False,
-             "noChanges": True,
-             "order": "DEPARTURE_DATE",
-             "offset": 0,
-             "limit": limit
+             "frecceOnly":False,
+              "regionalOnly":True,
+              "intercityOnly":False,
+              "tourismOnly":False,
+              "noChanges":True,
+              "order":"DEPARTURE_DATE",
+              "offset":0,
+              "limit":limit
         },
         "advancedSearchRequest": {
             "bestFare": False,
-            "bikeFilter": False
+            "bikeFilter":False
         }
     }
-
     try:
         response = requests.post(url, json=body)
-        return jsonify(response.json())
-    except Exception as e:
+        print("API response status:", response.status_code)  # Stampa lo status code
+        print("API raw response:", response.text)  # Stampa il contenuto ricevuto
+        response.raise_for_status()  # Solleva un'eccezione se c'è un errore HTTP
+        return jsonify(response.json())  # Converte e restituisce JSON
+    except requests.exceptions.RequestException as e:
+        print("Errore durante la richiesta:", e)
         return jsonify({"error": str(e)})
 
-@app.route('/get_stops', methods=['POST'])
-def get_stops():
-    data = request.get_json()  # Riceve i dati come JSON
-    solutionId = data.get("solutionId")
-    cartId = data.get("cartId")
-
-    # Usa i parametri nell'URL come query string per una richiesta GET
-    url = f"https://www.lefrecce.it/Channels.Website.BFF.WEB/website/stops?cartId={cartId}&solutionId={solutionId}"
-
-    try:
-        # Usa la richiesta GET invece di POST se non c'è bisogno di un corpo
-        response = requests.get(url)
-        return jsonify(response.json())  # Restituisce la risposta JSON
-    except Exception as e:
-        return jsonify({"error": str(e)})  # In caso di errore
-
-@app.route('/get_train_info', methods=['POST'])
-def get_train_info():
-    # Ricevi i dati dal front-end
-    data = request.get_json()
-    station_name_from = data.get("from")
-    station_name_to = data.get("to")
-    limit = data.get("limit")
-
-    # URL per la richiesta di soluzioni treni
-    url_trains = "https://www.lefrecce.it/Channels.Website.BFF.WEB/website/ticket/solutions"
-    
-    body_trains = {
-        "departureLocationId": stazioni.get(station_name_from, 830011112),  # Default se non trovato
-        "arrivalLocationId": stazioni.get(station_name_to, 830011112),
-        "departureTime": get_italy_current_time(),
-        "adults": 1,
-        "children": 0,
-        "criteria": {
-             "frecceOnly": False,
-             "regionalOnly": True,
-             "intercityOnly": False,
-             "tourismOnly": False,
-             "noChanges": True,
-             "order": "DEPARTURE_DATE",
-             "offset": 0,
-             "limit": limit
-        },
-        "advancedSearchRequest": {
-            "bestFare": False,
-            "bikeFilter": False
-        }
-    }
-
-    try:
-        # Prima richiesta per ottenere i treni
-        response_trains = requests.post(url_trains, json=body_trains)
-        
-        solutions = response_trains.json().get('solutions', [])
-        cartId = response_trains.json().get('cartId', [])
-
-        # Se non ci sono soluzioni, ritorna un errore
-        if not solutions:
-            return jsonify({"error": "No train solutions found."})
-
-        # Itera sulle soluzioni e raccogli i dati per ogni treno
-        result = []
-        for solution in solutions:
-            solution_data = solution.get('solution', {})
-            solutionId = solution_data.get('id')
-
-            # Seconda richiesta per ottenere le fermate
-            url_stops = f"https://www.lefrecce.it/Channels.Website.BFF.WEB/website/stops?cartId={cartId}&solutionId={solutionId}"
-            response_stops = requests.get(url_stops)
-            stops_data = response_stops.json()
-
-            # Verifica se ci sono fermate
-            isLocationPresent = any(stop['location']['id'] == 830011116 for stop in stops_data[0].get('stops', []))
-
-            # Prepara i dati da restituire
-            result.append({
-                "train_name": solution_data.get('trainName', 'N/A'),
-                "departure_time": solution_data.get('departureTime'),
-                "arrival_time": solution_data.get('arrivalTime'),
-                "price": solution_data.get('price', {}).get('amount', 'N/A'),
-                "is_stop_present": isLocationPresent
-            })
-
-        return jsonify(result)
-
-    except Exception as e:
-        return jsonify({"error": str(e)})
 
 
 if __name__ == "__main__":
